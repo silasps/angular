37e5a17780e54e9abfe1694d0ce33d7668dc2bbe
silasps <silaspereiras@gmail.com>
2021-03-01 23:35:51 -0300

    Inserting a church structure.

--- src/app/component/photo/photo.component.ts
+++ src/app/component/photo/photo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {Car, Engine, Radiator} from '../../interfaces/car';
+import {Casa} from '../../interfaces/casa';
 
 @Component({
   selector: 'app-photo',
@@ -27,32 +28,38 @@ export class PhotoComponent implements OnInit {
     return result;
   }
   generateData(){
-    let ferrari: Car = {} as Car;// {} Criando obj referenciando à car.ts
-    ferrari.name = 'Ferrari V8';
-    ferrari.motor = {} as Engine;
-    ferrari.motor.hp = 1;
-    ferrari.motor.valves = 10;
-    ferrari.motor.radiator = {} as Radiator;
-    ferrari.motor.radiator.waterBased = true;
+    // let ferrari: Car = new Car();// {} Criando obj referenciando à car.ts
+    // ferrari.name = 'Ferrari V8';
+    // ferrari.motor = {} as Engine;
+    // ferrari.motor.hp = 1;
+    // ferrari.motor.valves = 10;
+    // ferrari.motor.radiator = {} as Radiator;
+    // ferrari.motor.radiator.waterBased = true;
+    //
+    // let ferrari2: Car = {} as Car;
+    // ferrari2.name = 'Palio';
+    // ferrari2.motor = {} as Engine;
+    // ferrari2.motor.hp = 1;
+    // ferrari2.motor.valves = 10;
+    // ferrari2.motor.radiator = {} as Radiator;
+    // ferrari2.motor.radiator.waterBased = true;
+    // const listaCarros: Car[] = [];//Array de carros
+    // listaCarros.push(ferrari);
+    // listaCarros.push(ferrari2);
+    // listaCarros.push(ferrari);
+    // console.log(listaCarros);
+    // console.log(this.name);
+    // console.log(this.age);
+    // if (this.name == 'Bela' && this.age == 22){
+    // this.sentence = this.generateSentence(this.name, this.age);
+    // this.result = this.generateTwoPlusAge(this.age);
+    // }
+    const casaAmalia = {} as Casa;
 
-    let ferrari2: Car = {} as Car;
-    ferrari2.name = 'Palio';
-    ferrari2.motor = {} as Engine;
-    ferrari2.motor.hp = 1;
-    ferrari2.motor.valves = 10;
-    ferrari2.motor.radiator = {} as Radiator;
-    ferrari2.motor.radiator.waterBased = true;
-    const listaCarros: Car[] = [];//Array de carros
-    listaCarros.push(ferrari);
-    listaCarros.push(ferrari2);
-    listaCarros.push(ferrari);
-    console.log(listaCarros);
-    console.log(this.name);
-    console.log(this.age);
-    if (this.name == 'Bela' && this.age == 22){
-    this.sentence = this.generateSentence(this.name, this.age);
-    this.result = this.generateTwoPlusAge(this.age);
-    }
+    casaAmalia.donoCasa = 'Murilo';
+    casaAmalia.donoCasa = 'Silas';
+    const casaMurilo = {} as Casa;
+    casaMurilo.donoCasa = 'Gelise';
 }
   setName(name: any) {
     this.name = name.target.value;
